41abb7b8638c21ed0059fdb60827bed51dde5608
Sandip Chopkar <130152156+sandipchopkar95@users.noreply.github.com>
2024-06-01 19:38:05 +0000

    Commit 2

---
+++ First/01_Variables.js
@@ -0,0 +1,12 @@
+const accountId=123451           //can not use second variable with same name (to change the value)
+let accountEmail="test@gmail.com"  //can not use second variable with same name (to change the value)
+var accountPassword="12345"       //prefer not to use because of scope function
+accountCity="Indore"         //can be change if define second value with same variable name
+let accountState;         //not defined
+
+//const accountId=154321                  //will give compile time error
+//let accountEmail="sandip@gmail.com"   //will give compile time error
+var accountPassword="54321"
+accountCity="Indore"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])  //to print value in table
---
+++ First/02_DataType.js
@@ -0,0 +1,15 @@
+let name="Sandip"
+let age =28
+let isLoggedIn=false
+
+
+//number==> 2
+//bigint
+//string==>""
+//boolean==>true/false
+//null==> standalone value
+//undefined==>
+//symbol==>unique
+
+console.log(typeof undefined);
+console.log(typeof null);
