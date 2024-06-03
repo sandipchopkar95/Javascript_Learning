026491528a8e8397109a8a54705cd4d8f91b51b4
Sandip Chopkar <sandipchopkar789@gmail.com>
2024-06-04 00:59:40 +0530

    conversion

---
+++ First/04_Conversions.js
@@ -0,0 +1,25 @@
+let value=3
+let negValue=-value
+console.log(negValue)
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/2);
+//console.log(2%2);
+//console.log(2+2);
+
+console.log(1+"2")
+
+console.log("1"+2)  
+
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+console.log(+true)
+
+let gameCOunter=100
+gameCOunter++;
+console.log(gameCOunter)
+
+//===
+console.log("2"===2)
