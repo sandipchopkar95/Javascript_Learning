0113b92ce120423d82ca6cd598757c2bd79038ef
Sandip Chopkar <sandipchopkar789@gmail.com>
2024-06-05 01:02:14 +0530

    DatesHandeling

---
+++ First/07_DateHandling.js
@@ -0,0 +1,16 @@
+//Dates
+
+let myDate=new Date()
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toLocaleString())
+console.log(typeof myDate)
+
+//let myCreatedDate=new Date(2023,0,23)
+let myCreatedDate=new Date(2023,0,23,5,3)
+console.log(myCreatedDate.toLocaleString())
+
+let myTimeStamp=Date.now()
+console.log(myTimeStamp)
+console.log(myCreatedDate.getTime())
+console.log(Math.floor(Date.now()/1000))
