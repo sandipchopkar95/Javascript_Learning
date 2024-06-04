8ee1b2abd56f0685e036a3741b09ad3263b40468
Sandip Chopkar <sandipchopkar789@gmail.com>
2024-06-05 00:39:19 +0530

    math and num

---
+++ First/06_NumsAndMath.js
@@ -0,0 +1,31 @@
+const score=400
+
+const balance=new Number(100)
+//console.log(balance)
+
+//console.log(balance.toString().length)
+//console.log(balance.toFixed(1))
+
+const otherNumber=123.783
+//console.log(otherNumber.toPrecision(3))
+
+const hundreds=1000000
+//console.log(hundreds.toLocaleString('en-IN'))
+
+//Maths
+//console.log(Math)
+//console.log(Math.abs(-4))  //negative->positive ,positive->positive
+//console.log(Math.round(4.6))
+//console.log(Math.ceil(4.9))
+//console.log(Math.floor(4.9))
+//console.log(Math.min(2,5,7,9,1,0,3,6))
+//console.log(Math.max(2,5,7,9,1,0,3,6))
+
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+console.log((Math.random()*10)+1)
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
